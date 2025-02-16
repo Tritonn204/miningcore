b6541a761f7f749ce7b2b2f520b75d48126678fd
wombatlabs <36169687+wombatlabs@users.noreply.github.com>
2025-02-15 17:03:14 -0800

    Add PPS Payment Scheme

--- src/Miningcore/AutofacModule.cs
+++ src/Miningcore/AutofacModule.cs
@@ -166,6 +166,10 @@ public class AutofacModule : Module
             .Keyed<IPayoutScheme>(PayoutScheme.PPLNSBF)
             .SingleInstance();
 
+        builder.RegisterType<PPSPaymentScheme>()
+            .Keyed<IPayoutScheme>(PayoutScheme.PPS)
+            .SingleInstance();
+
         builder.RegisterType<SOLOPaymentScheme>()
             .Keyed<IPayoutScheme>(PayoutScheme.SOLO)
             .SingleInstance();
