89a3e72f8ecf6d98d230314db7d1af5558bb646f
wombatlabs <36169687+wombatlabs@users.noreply.github.com>
2025-02-15 17:11:43 -0800

    Add FPPS Payment Scheme

--- src/Miningcore/AutofacModule.cs
+++ src/Miningcore/AutofacModule.cs
@@ -170,6 +170,10 @@ public class AutofacModule : Module
             .Keyed<IPayoutScheme>(PayoutScheme.PPS)
             .SingleInstance();
 
+        builder.RegisterType<FPPSPaymentScheme>()
+            .Keyed<IPayoutScheme>(PayoutScheme.FPPS)
+            .SingleInstance();
+
         builder.RegisterType<SOLOPaymentScheme>()
             .Keyed<IPayoutScheme>(PayoutScheme.SOLO)
             .SingleInstance();
--- src/Miningcore/Configuration/ClusterConfig.cs
+++ src/Miningcore/Configuration/ClusterConfig.cs
@@ -874,6 +874,7 @@ public enum PayoutScheme
     PPS = 4,
     PPBS = 5,
     PPLNSBF = 6,
+    FPPS = 7,
 }
 
 public partial class ClusterLoggingConfig
